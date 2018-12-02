886a51733be2a1181da4449c9ee58702c157330c
mtuduri <matias_tuduri@hotmail.com>
2018-12-02 15:47:18 -0300

    header navigation

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
 
 const routes: Routes = [
-  {path: 'contact', loadChildren: './contact/contact.module#ContactModule'}
+  {path: "contact", loadChildren: "./contact/contact.module#ContactModule"}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- ui/src/app/core/core.module.ts
+++ ui/src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {MDBBootstrapModule} from "angular-bootstrap-md";
 import {FooterComponent} from "./layout/footer/footer.component";
+import {HeaderComponent} from "./layout/header/header.component";
+import {BreadcrumbComponent} from "./layout/breadcrumb/breadcrumb.component";
 
 @NgModule({
-  declarations: [FooterComponent],
-  exports: [FooterComponent],
+  declarations: [FooterComponent, HeaderComponent, BreadcrumbComponent],
+  exports: [FooterComponent, HeaderComponent, BreadcrumbComponent],
   imports: [
     CommonModule,
     MDBBootstrapModule.forRoot(),
