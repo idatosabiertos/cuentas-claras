97f4391f4426850a8279c865402af45b43be16ab
Jonathan Amigo <jonathan.amigo.torres@gmail.com>
2019-01-27 18:02:37 -0300

    Fix staticfiles

--- CuentasClaras/Startup.cs
+++ CuentasClaras/Startup.cs
@@ -24,7 +24,7 @@ namespace CuentasClaras
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "wwwroot";
             });
         }
 
