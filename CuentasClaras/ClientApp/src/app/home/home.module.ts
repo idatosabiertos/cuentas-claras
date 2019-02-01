8b4a91a1099b7aa045fe4b4e258e8f601aa9c2bf
mtuduri <matias_tuduri@hotmail.com>
2019-01-31 22:21:36 -0300

    ngx datatable

--- CuentasClaras/ClientApp/src/app/home/home.module.ts
+++ CuentasClaras/ClientApp/src/app/home/home.module.ts
@@ -4,13 +4,16 @@ import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MDBBootstrapModule
+    MDBBootstrapModule,
+    NgxDatatableModule
   ]
 })
 export class HomeModule { }
