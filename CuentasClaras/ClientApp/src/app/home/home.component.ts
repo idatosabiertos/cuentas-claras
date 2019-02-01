1e31f26b9386a1248131fa3ae3a3e500cfcc3092
mtuduri <matias_tuduri@hotmail.com>
2019-02-01 20:35:39 -0300

    ngx-graph

--- CuentasClaras/ClientApp/src/app/home/home.component.ts
+++ CuentasClaras/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as shape from 'd3-shape';
 
 @Component({
   selector: 'app-home',
@@ -6,20 +7,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+
+  /// TABLES
   rows = [
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+    {name: 'Austin', gender: 'Male', company: 'Swimlane'},
+    {name: 'Dany', gender: 'Male', company: 'KFC'},
+    {name: 'Molly', gender: 'Female', company: 'Burger King'},
   ];
   columns = [
-    { prop: 'name' },
-    { name: 'Gender' },
-    { name: 'Company' }
+    {prop: 'name'},
+    {name: 'Gender'},
+    {name: 'Company'}
   ];
 
-  constructor() { }
 
-  ngOnInit() {
+  // NETWORK GRAPH
+
+  hierarchialGraph = {nodes: [], links: []}
+  curve = shape.curveBundle.beta(1);
+
+  constructor() {
   }
+
+  public ngOnInit(): void {
+    this.showGraph();
+  }
+
+  showGraph() {
+    this.hierarchialGraph.nodes = [
+      {
+        id: 'start',
+        label: 'scan',
+        position: 'x0'
+      }, {
+        id: '1',
+        label: 'Event#a',
+        position: 'x1'
+      }, {
+        id: '2',
+        label: 'Event#x',
+        position: 'x2'
+      }, {
+        id: '3',
+        label: 'Event#b',
+        position: 'x3'
+      }, {
+        id: '4',
+        label: 'Event#c',
+        position: 'x4'
+      }, {
+        id: '5',
+        label: 'Event#y',
+        position: 'x5'
+      }, {
+        id: '6',
+        label: 'Event#z',
+        position: 'x6'
+      }
+    ];
+
+    this.hierarchialGraph.links = [
+      {
+        source: 'start',
+        target: '1',
+        label: 'Process#1'
+      }, {
+        source: 'start',
+        target: '2',
+        label: 'Process#2'
+      }, {
+        source: '1',
+        target: '3',
+        label: 'Process#3'
+      }, {
+        source: '2',
+        target: '4',
+        label: 'Process#4'
+      }, {
+        source: '2',
+        target: '6',
+        label: 'Process#6'
+      }, {
+        source: '3',
+        target: '5'
+      }
+    ];
+
+  }
+
 
 }
--- CuentasClaras/ClientApp/src/app/home/home.module.ts
+++ CuentasClaras/ClientApp/src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { NgxDatatableModule } from '@swimlane/ngx-datatable';
     CommonModule,
     HomeRoutingModule,
     MDBBootstrapModule,
-    NgxDatatableModule
+    NgxDatatableModule,
+    NgxGraphModule,
+    NgxChartsModule
   ]
 })
 export class HomeModule { }
