8363fa84c646c8917967f77c589a83247cbf5b42
mtuduri <matias_tuduri@hotmail.com>
2019-02-02 16:03:10 -0300

    news

--- CuentasClaras/ClientApp/src/app/news/news.component.ts
+++ CuentasClaras/ClientApp/src/app/news/news.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  public news = [
+    {
+      img:'https://mdbootstrap.com/img/Photos/Others/images/49.jpg',
+      title: 'Titulo de la Nota',
+      summary: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, psam voluptatem quia consectetur.',
+      author: 'Jessica Clark',
+      date: '12/04/2018'
+    },
+    {
+      img:'https://mdbootstrap.com/img/Photos/Others/images/31.jpg',
+      title: 'Titulo de la Nota',
+      summary: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, psam voluptatem quia consectetur.',
+      author: 'Jessica Clark',
+      date: '12/04/2018'
+    },
+    {
+      img:'https://mdbootstrap.com/img/Photos/Others/images/52.jpg',
+      title: 'Titulo de la Nota',
+      summary: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, psam voluptatem quia consectetur.',
+      author: 'Jessica Clark',
+      date: '12/04/2018'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- CuentasClaras/ClientApp/src/app/news/news.module.ts
+++ CuentasClaras/ClientApp/src/app/news/news.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import { NewsRoutingModule } from './news-routing.module';
 import { NewsComponent } from './news.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
-  declarations: [NewsComponent],
+  declarations: [NewsComponent, PostComponent],
   imports: [
     CommonModule,
-    NewsRoutingModule
+    NewsRoutingModule,
+    MDBBootstrapModule
   ]
 })
 export class NewsModule { }
