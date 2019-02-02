57752ae031e5a015d642ed63fa2dd8a18213badc
mtuduri <matias_tuduri@hotmail.com>
2019-02-02 15:39:47 -0300

    buying index

--- CuentasClaras/ClientApp/src/app/buying-index/buying-index.component.ts
+++ CuentasClaras/ClientApp/src/app/buying-index/buying-index.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyingIndexComponent implements OnInit {
 
-  constructor() { }
+  public data = [
+    {title: 'Presidencia', rating: 4.5, description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'},
+    {
+      title: 'Ministerio de Economia',
+      rating: 4.0,
+      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
+    },
+    {
+      title: 'Presidencia',
+      rating: 3.5,
+      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
+    },
+    {
+      title: 'Ministerio de Salud Publica',
+      rating: 2.5,
+      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
+    },
+    {
+      title: 'Ministerio de Transporte',
+      rating: 2.0,
+      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
+    },
+    {
+      title: 'Intendencia Municipal de Montevideo',
+      rating: 1.5,
+      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
+    }
+  ];
+  public indexDataGroup = [];
+
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.splitArray();
+  }
+
+  private splitArray() {
+    const size = 4;
+    while (this.data.length > 0) {
+      this.indexDataGroup.push(this.data.splice(0, size));
+    }
   }
 
 }
--- CuentasClaras/ClientApp/src/app/buying-index/buying-index.module.ts
+++ CuentasClaras/ClientApp/src/app/buying-index/buying-index.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import { BuyingIndexRoutingModule } from './buying-index-routing.module';
 import { BuyingIndexComponent } from './buying-index.component';
+import { StarRatingModule } from 'angular-star-rating';
 
 @NgModule({
   declarations: [BuyingIndexComponent],
   imports: [
     CommonModule,
-    BuyingIndexRoutingModule
+    BuyingIndexRoutingModule,
+    MDBBootstrapModule,
+    StarRatingModule.forRoot()
   ]
 })
 export class BuyingIndexModule { }
