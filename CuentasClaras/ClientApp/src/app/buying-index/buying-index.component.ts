d4089f2ae1f5cd5baee182092d5a55bd21ab2ffe
mtuduri <matias_tuduri@hotmail.com>
2019-02-13 20:52:00 -0300

    index as a table instead of cards

--- CuentasClaras/ClientApp/src/app/buying-index/buying-index.component.ts
+++ CuentasClaras/ClientApp/src/app/buying-index/buying-index.component.ts
@@ -1,54 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-buying-index',
   templateUrl: './buying-index.component.html',
   styleUrls: ['./buying-index.component.scss']
 })
-export class BuyingIndexComponent implements OnInit {
-
-  public data = [
-    {title: 'Presidencia', rating: 4.5, description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'},
+export class BuyingIndexComponent {
+  columns = [
+    {prop: 'entity', name: 'Ente'},
+  ];
+  rows = [
     {
-      title: 'Ministerio de Economia',
+      entity: 'Presidencia',
+      rating: 4.5,
+    },
+    {
+      entity: 'Ministerio de Economia',
       rating: 4.0,
-      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
     },
     {
-      title: 'Ministerio de Industria, Energía y Minería',
+      entity: 'Ministerio de Industria, Energía y Minería',
       rating: 3.5,
-      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
     },
     {
-      title: 'Ministerio de Salud Publica',
+      entity: 'Ministerio de Salud Publica',
       rating: 2.5,
-      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
     },
     {
-      title: 'Ministerio de Transporte',
+      entity: 'Ministerio de Transporte',
       rating: 2.0,
-      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
     },
     {
-      title: 'Intendencia Municipal de Montevideo',
+      entity: 'Intendencia Municipal de Montevideo',
       rating: 1.5,
-      description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit...'
     }
   ];
-  public indexDataGroup = [];
-
-  constructor() {
-  }
-
-  ngOnInit() {
-    this.splitArray();
-  }
-
-  private splitArray() {
-    const size = 2;
-    while (this.data.length > 0) {
-      this.indexDataGroup.push(this.data.splice(0, size));
-    }
-  }
 
 }
