a72053c7be3bc298daa49affa288954919986580
mtuduri <matias_tuduri@hotmail.com>
2019-05-26 16:34:51 -0300

    year filter fix

--- CuentasClaras/ClientApp/src/app/shared/year-filter/year-filter.module.ts
+++ CuentasClaras/ClientApp/src/app/shared/year-filter/year-filter.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YearFilterComponent } from './year-filter.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   exports:[YearFilterComponent],
   declarations: [YearFilterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MDBBootstrapModule
   ]
 })
 export class YearFilterModule { }
