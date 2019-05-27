6cad15d00cdcf3d9e28201150196452b02faba43
Jonathan Amigo <jonathan.amigo.torres@gmail.com>
2019-05-27 21:22:41 +0100

    TopProduct

---
+++ CuentasClaras/Controllers/Organisation/BuyerController.cs
@@ -0,0 +1,70 @@
+using CuentasClaras.Api.Organisation;
+using CuentasClaras.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CuentasClaras.Controllers
+{
+    [Route("api/buyer")]
+    [ApiController]
+    public class BuyerController : ControllerBase
+    {
+        private CuentasClarasContext db;
+
+        public BuyerController(CuentasClarasContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [OutputCache(Duration = 600)]
+        public List<Buyer> GetBuyers()
+        {
+            return db.Buyers.ToList();
+        }
+
+        [HttpGet]
+        [Route("{id}/stats")]
+        public BuyerDTO GetBuyer([FromRoute(Name = "id")] int id, [FromQuery(Name = "years")] string years)
+        {
+            var yearsList = years.Split(",");
+
+            var buyer = db.Buyers
+                .Where(s => s.BuyerId == id)
+                .Include(x => x.Releases)
+                    .ThenInclude(r => r.ReleaseItems)
+                        .ThenInclude(ri => ri.ReleaseItemClassification)
+                .Include(x => x.Releases)
+                    .ThenInclude(r => r.Supplier)
+                .Include(x => x.OrganisationIndexes)
+                .SingleOrDefault();
+
+            /*
+            var query = (from b in db.Buyers
+                         where b.BuyerId == id
+                         select new
+                         {
+                             BuyerExternalId = b.BuyerExternalId,
+                             BuyerId = b.BuyerId,
+                             Name = b.Name,
+                             Type = b.Type,
+                             OrganisationIndexes = from o in db.OrganisationIndexes
+                                                   where o.BuyerId == b.BuyerId
+                                                   select new OrganisationIndex
+                                                   {
+                                                       OrganisationId = o.OrganisationId,
+                                                       AccumulationOfSuppliersByOrganisation = o.AccumulationOfSuppliersByOrganisation,
+                                                       BuyerId = o.BuyerId,
+                                                       CompletedInfo = o.CompletedInfo
+                                                   },
+                             Releases = b.Releases.Where(x => yearsList.Contains(x.DataSource))
+                         }).First();*/
+
+            return BuyerDTO.From(buyer, yearsList);
+        }
+    }
+}
--- CuentasClaras/Controllers/Organisation/OrganisationController.cs
+++ CuentasClaras/Controllers/Organisation/OrganisationController.cs
@@ -45,46 +45,5 @@ namespace CuentasClaras.Controllers
 
             return SupplierDTO.From(supplier);
         }
-
-        [HttpGet]
-        [Route("buyers/stats/{id}")]
-        public BuyerDTO GetBuyer([FromRoute(Name = "id")] int id, [FromQuery(Name = "years")] string years)
-        {
-            var yearsList = years.Split(",");
-
-            var buyer = db.Buyers
-                .Where(s => s.BuyerId == id)
-                .Include(x => x.Releases)
-                    .ThenInclude(r => r.ReleaseItems)
-                        .ThenInclude(ri => ri.ReleaseItemClassification)
-                .Include(x => x.Releases)
-                    .ThenInclude(r => r.Supplier)
-                .Include(x => x.OrganisationIndexes)
-                .SingleOrDefault();
-
-            /*
-            var query = (from b in db.Buyers
-                         where b.BuyerId == id
-                         select new
-                         {
-                             BuyerExternalId = b.BuyerExternalId,
-                             BuyerId = b.BuyerId,
-                             Name = b.Name,
-                             Type = b.Type,
-                             OrganisationIndexes = from o in db.OrganisationIndexes
-                                                   where o.BuyerId == b.BuyerId
-                                                   select new OrganisationIndex
-                                                   {
-                                                       OrganisationId = o.OrganisationId,
-                                                       AccumulationOfSuppliersByOrganisation = o.AccumulationOfSuppliersByOrganisation,
-                                                       BuyerId = o.BuyerId,
-                                                       CompletedInfo = o.CompletedInfo
-                                                   },
-                             Releases = b.Releases.Where(x => yearsList.Contains(x.DataSource))
-                         }).First();*/
-
-            return BuyerDTO.From(buyer, yearsList);
-        }
-
     }
 }
