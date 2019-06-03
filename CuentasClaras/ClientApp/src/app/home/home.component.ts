5cc5cab295a289c31a28017fcf55bce0568f737e
mtuduri <matias_tuduri@hotmail.com>
2019-06-03 19:21:33 -0300

    improved search performance

--- CuentasClaras/ClientApp/src/app/home/home.component.ts
+++ CuentasClaras/ClientApp/src/app/home/home.component.ts
@@ -35,6 +35,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   itemsListDDconfig = {
     displayKey: 'description', //if objects array passed which key to be displayed defaults to description
     search: true,//true/false for the search functionlity defaults to false,
+    limitTo: 5,
     height: 'auto', //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
     placeholder: 'Seleccione un artículo', // text to be displayed when no item is selected defaults to Select,
     moreText: 'more', // text to be displayed whenmore than one items are selected like Option 1 + 5 more
